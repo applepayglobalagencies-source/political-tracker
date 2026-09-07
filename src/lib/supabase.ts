38ad28d422ad225b5729f9c4d773260d4f5c8d0c
Your Name <you@example.com>
2026-09-07 14:58:21 +0300

    fix bom tsconfig + supabase dep

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,2 @@
-import { createClient } from '@supabase/supabase-js'; export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
